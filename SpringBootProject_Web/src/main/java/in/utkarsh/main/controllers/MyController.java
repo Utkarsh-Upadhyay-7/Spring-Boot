53b609d0aa945c95d50103f4154d9427cd92ed5c
Utkarsh-Upadhyay-7 <123894031+Utkarsh-Upadhyay-7@users.noreply.github.com>
2025-12-18 23:57:02 +0530

    Add files via upload

---
+++ SpringBootProject_Web/src/main/java/in/utkarsh/main/controllers/MyController.java
@@ -0,0 +1,14 @@
+package in.utkarsh.main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+	@GetMapping("/")
+	@ResponseBody
+	public String printHello() {
+		return "Hello Utkarsh Ji";
+	}
+}
